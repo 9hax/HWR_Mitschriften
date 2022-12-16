9b8c8d2902d6f8f98ae6639562fe7b988f4be837
9hax <me@9hax.net>
2022-12-16 09:57:02 +0100

    circles

--- sem1/einfuehrung_in_die_programmierung/extras/ue07/graphics/imaging/imaging.go
+++ sem1/einfuehrung_in_die_programmierung/extras/ue07/graphics/imaging/imaging.go
@@ -8,7 +8,13 @@ import (
 )
 
 func CreateBasicGif(width, height int) *image.Paletted {
-	palette := []color.Color{color.Transparent, color.White, color.Black}
+	palette := []color.Color{
+		color.Transparent,
+		color.White,
+		color.Black,
+		color.RGBA{0xff, 0x00, 0x00, 0xff},
+		color.RGBA{0x00, 0xff, 0x00, 0xff},
+		color.RGBA{0x00, 0x00, 0xff, 0xff}}
 	rect := image.Rect(0, 0, width, height)
 	img := image.NewPaletted(rect, palette)
 	return img
@@ -30,23 +36,88 @@ func SetPixel(img *image.Paletted, x int, y int, col uint8) {
 }
 
 func DrawLine(img *image.Paletted, x0 int, y0 int, x1 int, y1 int, col uint8) {
+	// Find the difference between the x and y coordinates
+	dx := abs(x1 - x0)
+	dy := abs(y1 - y0)
+
+	// Determine which coordinate has the greatest difference
+	var sx, sy int
+	if x0 < x1 {
+		sx = 1
+	} else {
+		sx = -1
+	}
+	if y0 < y1 {
+		sy = 1
+	} else {
+		sy = -1
+	}
+
+	// Initialize the error value to 0
+	err := dx - dy
+
+	for {
+		// Set the pixel at the current position
+		SetPixel(img, x0, y0, col)
+
+		// If the current position is the same as the ending position, the line is drawn
+		if x0 == x1 && y0 == y1 {
+			break
+		}
+
+		// Calculate the new error value
+		e2 := 2 * err
+		if e2 > -dy {
+			err -= dy
+			x0 += sx
+		}
+		if e2 < dx {
+			err += dx
+			y0 += sy
+		}
+	}
+}
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+/*
+func DrawLine(img *image.Paletted, x0 int, y0 int, x1 int, y1 int, col uint8) {
 	dx := x1 - x0
 	dy := y1 - y0
 	D := 2*dy - dx
 	y := y0
 	for x := x0; x < x1; x++ {
-		img.SetColorIndex(x, y, col)
+		println("Setting Pixel at ", x, ",", y, "to", col)
+		SetPixel(img, x, y, col)
 		if D > 0 {
 			y = y + 1
 			D = D - 2*dx
 		}
 		D = D + 2*dy
 	}
-}
+}*/
 
 func DrawRect(img *image.Paletted, x0, y0, x1, y1 int, col uint8) {
 	DrawLine(img, x0, y0, x1, y0, col)
 	DrawLine(img, x0, y1, x1, y1, col)
 	DrawLine(img, x1, y0, x1, y1, col)
 	DrawLine(img, x0, y0, x0, y1, col)
+}
+
+func DrawCircle(img *image.Paletted, x, y, dia int, col uint8) {
+	// Calculate the radius of the circle
+	radius := dia / 2
+	// Iterate over the bounding box of the circle
+	for i := x - radius; i <= x+radius; i++ {
+		for j := y - radius; j <= y+radius; j++ {
+			// If the point is close to the radius, set the pixel
+			if abs((i-x)*(i-x)+(j-y)*(j-y)-(radius*radius)) <= radius {
+				SetPixel(img, i, j, col)
+			}
+		}
+	}
 }
--- sem1/einfuehrung_in_die_programmierung/extras/ue07/graphics/main.go
+++ sem1/einfuehrung_in_die_programmierung/extras/ue07/graphics/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	i "graphics/imaging"
+	"image"
 )
 
 func main() {
@@ -10,8 +11,37 @@ func main() {
 	img := i.CreateBasicGif(100, 100)
 	i.SetPixel(img, 50, 50, 1)
 	i.SetPixel(img, 25, 25, 1)
-	i.DrawLine(img, 10, 10, 10, 90, 1)
+	i.DrawLine(img, 15, 10, 40, 0, 1)
+
+	drawNikolaus(img, 10, 40, 3)
+	drawFunnyCircles(img)
 
 	i.WriteImage(img)
 
+}
+
+func drawNikolaus(img *image.Paletted, x, y int, col uint8) {
+	// Box
+	/*
+		i.DrawLine(img, x, y+5, x+10, y+5, col)
+		i.DrawLine(img, x, y+15, x+10, y+15, col)
+		i.DrawLine(img, x, y+5, x, y+15, col)
+		i.DrawLine(img, x+10, y+5, x+10, y+15, col)
+	*/
+	i.DrawRect(img, x, y+5, x+10, y+15, col)
+
+	i.DrawLine(img, x, y+5, x+10, y+15, col)
+	i.DrawLine(img, x, y+15, x+10, y+5, col)
+
+	i.DrawLine(img, x, y+5, x+5, y, col)
+	i.DrawLine(img, x+10, y+5, x+5, y, col)
+}
+
+func drawFunnyCircles(img *image.Paletted) {
+	i.DrawCircle(img, 40, 50, 20, 1)
+	i.DrawCircle(img, 70, 70, 70, 1)
+	i.DrawCircle(img, 70, 70, 60, 2)
+	i.DrawCircle(img, 70, 70, 50, 3)
+	i.DrawCircle(img, 70, 70, 40, 4)
+	i.DrawCircle(img, 70, 70, 30, 5)
 }
